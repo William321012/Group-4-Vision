67b521ba084b900abf831971204509d6e5f1cdf4
William Lin <william.lin48@bcmail.cuny.edu>
2023-04-15 17:06:02 -0400

    Revert "Merge branch 'main' into Zheng"
    This reverts commit dfb7d9dfbf9dda4412e221683f2ccb819a3c6a3d, reversing
    changes made to b40c2f5c3f34c21ed0c5b098679eb4c9b3fcadae.

--- group4_vision/Assets/Scripts/CameraFollow.cs
+++ group4_vision/Assets/Scripts/CameraFollow.cs
@@ -9,6 +9,7 @@ public class CameraFollow : MonoBehaviour
     public float yOffSet = 1f;
     public Transform target;
 
+    // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffSet, cameraZPosition);
